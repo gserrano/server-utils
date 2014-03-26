fa380c7ae0ed832561156f427a0bb16460338036
Guilherme Serrano <contato@guilhermeserrano.com.br>
2014-03-26 10:47:18 -0300

    create-site.sh : create virtual host on Apache ans folders on /var/www/domain

---
+++ create-site.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+################
+# Script for creating Virtual Servers On Apache2
+# Check for the correct parameters
+if [ $# -lt 2 ]; then
+	echo 'Você precisa passar o USER e o DOMÍNIO a ser criado'
+	echo 'sh create-site user DOMAIN'
+	echo ''
+	echo 'Ex: sh create-site www gssolutions.com.br'
+	echo 'Cria gssolutions.com.br em /var/www/gssolutions.com.br/public'
+	exit 0
+fi	
+
+# Assign Variables
+DOMAIN=$2
+USER=$1
+
+# Create the Directory which will contain your Virtual Site
+if [ ! -d  /var/www/${DOMAIN} ]; then
+	mkdir /var/www/${DOMAIN}
+	echo 'create /var/www/${DOMAIN}'
+fi
+if [ ! -d  /var/www/${DOMAIN}/public ]; then
+	mkdir /var/www/${DOMAIN}/public
+	echo 'create /var/www/${DOMAIN}/public'
+fi
+if [ ! -d  /var/www/${DOMAIN}/logs ]; then
+	mkdir /var/www/${DOMAIN}/logs
+	echo 'create /var/www/${DOMAIN}/logs'
+fi
+
+chown -R ${USER}:${USER} /var/www/${DOMAIN}/public
+chown -R ${USER}:${USER} /var/www/${DOMAIN}/logs
+
+# Create the Config file for your virtual site
+
+#backup do /etc/hosts
+#cp /etc/hosts /etc/hosts.original
+#echo "127.0.0.1\t${DOMAIN}">> /etc/hosts
+
+#echo "#NameVirtualHost * "> /etc/apache2/sites-available/${DOMAIN}
+echo "<VirtualHost *:80>">> /etc/apache2/sites-available/${DOMAIN}
+
+echo	"	ServerAdmin contato@gssolutions.com.br" >> /etc/apache2/sites-available/${DOMAIN}
+echo	"	DocumentRoot \"/var/www/${DOMAIN}/public\"" >>/etc/apache2/sites-available/${DOMAIN}
+echo	"	ServerName  ${DOMAIN}" >> /etc/apache2/sites-available/${DOMAIN}
+echo	"	ServerAlias www.${DOMAIN}" >> /etc/apache2/sites-available/${DOMAIN}
+
+echo 	"" >>/etc/apache2/sites-available/${DOMAIN}	
+
+echo	'	<Directory />'>>/etc/apache2/sites-available/${DOMAIN}
+echo	'		Options FollowSymLinks'>>/etc/apache2/sites-available/${DOMAIN}
+echo	'		AllowOverride None'>>/etc/apache2/sites-available/${DOMAIN}
+echo	'	</Directory>'>>/etc/apache2/sites-available/${DOMAIN}
+
+echo 	"" >>/etc/apache2/sites-available/${DOMAIN}
+
+echo	"	<Directory /var/www/${DOMAIN}/public>">>/etc/apache2/sites-available/${DOMAIN}
+echo	'		Options Indexes FollowSymLinks MultiViews'>>/etc/apache2/sites-available/${DOMAIN}
+echo	'		AllowOverride All'>>/etc/apache2/sites-available/${DOMAIN}
+echo	'		Order allow,deny'>>/etc/apache2/sites-available/${DOMAIN}
+echo	'		allow from all'>>/etc/apache2/sites-available/${DOMAIN}
+echo	'	</Directory>'>>/etc/apache2/sites-available/${DOMAIN}
+
+echo 	"" >>/etc/apache2/sites-available/${DOMAIN}
+
+echo	"	ErrorLog /var/www/${DOMAIN}/logs/error.log">>/etc/apache2/sites-available/${DOMAIN}
+echo	'	LogLevel warn'>>/etc/apache2/sites-available/${DOMAIN}
+echo	"	CustomLog /var/www/${DOMAIN}/logs/access.log combined">>/etc/apache2/sites-available/${DOMAIN}
+#echo	'	ServerSignature On'>>/etc/apache2/sites-available/${DOMAIN}
+
+echo '</VirtualHost>'>>/etc/apache2/sites-available/${DOMAIN}
+
+if [ ! -L /etc/apache2/sites-enabled/${DOMAIN} ]; then 
+	ln -s /etc/apache2/sites-available/${DOMAIN} /etc/apache2/sites-enabled/ 
+fi
+
+#Restart apache
+sudo service apache2 restart
